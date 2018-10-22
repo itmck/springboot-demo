015cbd662b920c5813c4ccbcfe35c25a37909ba9
itmck <347451331@qq.com>
2018-10-22 10:30:42 +0800

    spring-boot简单使用

--- src/main/java/com/qf/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/qf/springbootdemo/SpringBootDemoApplication.java
@@ -1,11 +1,11 @@
 package com.qf.springbootdemo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(value = "com.qf.springbootdemo.dao")//扫描这个包下的mapper
+//在要扫描的mapper上加上@Mapper注解.所以就不用在此使用@MapperScan.
+//@MapperScan(value = "com.qf.springbootdemo.dao")//扫描这个包下的mapper
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
